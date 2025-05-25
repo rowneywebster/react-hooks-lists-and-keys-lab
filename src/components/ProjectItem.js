972e2bf1a2f02b7fec2ecd6c1126c5344196d1d2
Rowney Ouma <oumarowney@gmail.com>
2025-05-25 16:26:16 +0300

    completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,6 +6,11 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+      {technologies.map((tech, index) => (
+          <span key={index} style={{ marginRight: "8px" }}>
+            {tech}
+          </span>
+        ))}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
